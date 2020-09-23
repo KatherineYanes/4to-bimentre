9116a84e8b5aa934dc24b6133bdb2a7977335948
KatherineYanes <62476969+KatherineYanes@users.noreply.github.com>
2020-09-23 08:19:36 -0600

    Menú

---
+++ Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tercera_prueba
+{
+    class Menu
+    {
+        private int num = 0;
+
+
+
+        public int M()
+        {
+            Console.WriteLine("Opciones de operaciones");
+            Console.WriteLine("_______________________");
+            Console.WriteLine("1.Suma");
+            Console.WriteLine("2.Resta");
+            Console.WriteLine("3.Multiplicacion");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("5.Potencia");
+            Console.WriteLine("6.Raiz");
+            Console.WriteLine("7.Salir");
+            Console.Write("Usted elegio la opcion: ");
+            try
+            {
+                num = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Por favor, intente de nuevo");
+            }
+
+            Console.Clear();
+            return num;
+
+        }
+    }
+}
